408a228c90aefe5d5a5922ba1dbb8f31dddd9f57
Kervins Valcourt <kervins.valcourtz@gmail.com>
2017-01-04 14:21:16 -0800

    created a collectionView with its collectionViewLayout. This will show the list of locations around the user.

--- CrushAndLovelyCodingTest/ListCollectionView.swift
+++ CrushAndLovelyCodingTest/ListCollectionView.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2017 Crush and Lovely. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class ListCollectionView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: ListCollectionView.provideCollectionViewLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    class func provideCollectionViewLayout() -> UICollectionViewLayout {
+        let screenWidth = UIScreen.main.bounds.size.width
+        let viewLayout = UICollectionViewFlowLayout()
+        viewLayout.scrollDirection = .vertical
+        viewLayout.itemSize = CGSize(width: screenWidth - 13.3, height: 73.5)
+        viewLayout.minimumInteritemSpacing = 3
+        viewLayout.minimumLineSpacing = 3
+        viewLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        return viewLayout
+    }
+}
