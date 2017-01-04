2c9b7cb23a95784bd2acca3c757049324f3abe74
Kervins Valcourt <kervins.valcourtz@gmail.com>
2017-01-04 14:18:26 -0800

    this is a constant file for the yelp secret, yelp AppID, and users current location variable.

--- CrushAndLovelyCodingTest/Constants.swift
+++ CrushAndLovelyCodingTest/Constants.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+import CoreLocation
+
+var yelpAppID = "Mc6I70Mzu9ciRHXddqpUUw"
+var yelpAppSecret = "MxqdbWR58WE06XW0q1UqOWBHAPp6HTmvITgAB7TFDX9khqWTJEz2vtaRm1F8hH5A"
+
+var currentUserLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
