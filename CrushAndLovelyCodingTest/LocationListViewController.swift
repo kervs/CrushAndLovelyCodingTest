66e84ecc89a4fc99bdc5f5142a52759f02a96749
Kervins Valcourt <kervins.valcourtz@gmail.com>
2017-01-04 14:27:20 -0800

    created locations List VC and map view VCs. Both takes a BrowseViewModel and displace the data that it is given from the viewModel respectively.

--- CrushAndLovelyCodingTest/LocationListViewController.swift
+++ CrushAndLovelyCodingTest/LocationListViewController.swift
@@ -6,4 +6,92 @@
 //  Copyright © 2017 Crush and Lovely. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import YelpAPI
+import Alamofire
+import AlamofireImage
+
+let cellReuseIdentifer = "cellReuseIdentifier"
+
+class LocationListViewController: BaseViewController,
+    UICollectionViewDelegate,
+    UICollectionViewDataSource,
+    UICollectionViewDelegateFlowLayout {
+    private var locationListView: ListCollectionView
+
+    override init(viewModel: BrowseViewModel = BrowseViewModel()) {
+        locationListView = ListCollectionView()
+        locationListView.translatesAutoresizingMaskIntoConstraints = false
+        locationListView.backgroundColor = UIColor.white
+
+        super.init(viewModel: viewModel)
+
+        locationListView.dataSource = self
+        locationListView.delegate = self
+        locationListView.register(LoactionCollectionCell.self, forCellWithReuseIdentifier: cellReuseIdentifer)
+
+        view.insertSubview(locationListView, belowSubview: reloadButton)
+
+        setupLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func setupLayout() {
+        super.setupLayout()
+        
+        locationListView.snp.makeConstraints { view -> Void in
+            view.top.equalToSuperview()
+            view.bottom.equalToSuperview()
+            view.left.equalToSuperview()
+            view.right.equalToSuperview()
+        }
+
+    }
+
+    // MARK: UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.locations.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if viewModel.locations.count == 0 {
+            return UICollectionViewCell()
+        }
+
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifer, for: indexPath as IndexPath) as? LoactionCollectionCell,
+            let location = viewModel.locations[indexPath.row]  else {
+                return UICollectionViewCell()
+        }
+
+        cell.titleLabel.text = location.name
+
+        if let imageURL = location.imageURL {
+            Alamofire.request(imageURL.absoluteString).responseImage { response in
+                debugPrint(response)
+
+                if let image = response.result.value {
+                   cell.locationImage.image = image
+                }
+            }
+        }
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let location = viewModel.locations[indexPath.row]  else {
+            return
+        }
+
+        let vc = LocationDetailViewController(location)
+        vc.view.backgroundColor = UIColor.white
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    override func browseViewModelDidUpdateLocation(_ browseViewModel: BrowseViewModel, location: [YLPBusiness]) {
+        locationListView.reloadData()
+    }
+}
--- CrushAndLovelyCodingTest/MapViewController.swift
+++ CrushAndLovelyCodingTest/MapViewController.swift
@@ -6,4 +6,64 @@
 //  Copyright © 2017 Crush and Lovely. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import YelpAPI
+import SnapKit
+import MapKit
+
+class MapViewController: BaseViewController {
+    private var mapView: MKMapView
+
+    override init(viewModel: BrowseViewModel = BrowseViewModel()) {
+        mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+
+        super.init(viewModel: viewModel)
+
+        view.insertSubview(mapView, belowSubview: reloadButton)
+
+        setupLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func setupLayout() {
+        super.setupLayout()
+        
+        mapView.snp.makeConstraints { view -> Void in
+            view.top.equalToSuperview()
+            view.bottom.equalToSuperview()
+            view.left.equalToSuperview()
+            view.right.equalToSuperview()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        reloadMapData()
+    }
+
+    func reloadMapData() {
+        var locationAnnotation: [MKPointAnnotation] = []
+
+        for item in viewModel.locations {
+            if let business = item,
+                let latitude = business.location.coordinate?.latitude,
+                let longitude = business.location.coordinate?.longitude {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+
+                locationAnnotation.append(annotation)
+            }
+        }
+
+        mapView.showAnnotations(locationAnnotation, animated: true)
+    }
+    
+    override func browseViewModelDidUpdateLocation(_ browseViewModel: BrowseViewModel, location: [YLPBusiness]) {
+        reloadMapData()
+    }
+}
