292aa3c435110ccb8aab3d6d4514762ab7bcf0ab
Kervins Valcourt <kervins.valcourtz@gmail.com>
2017-01-04 14:13:01 -0800

    created a RootViewController that will house the two VCs map view and list view. user will be able to switch between VCs at will.

--- CrushAndLovelyCodingTest/RootViewController.swift
+++ CrushAndLovelyCodingTest/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  BrowseTabBarViewController.swift
+//  RootViewController.swift
 //  CrushAndLovelyCodingTest
 //
 //  Created by Kervins Valcourt on 1/3/17.
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class BrowseTabBarViewController: UIViewController {
+class RootViewController: UITabBarController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        setupTabBarItems()
+    }
+
+    func setupTabBarItems() {
+        let mapVC = UINavigationController(rootViewController: MapViewController())
+        let listVC = UINavigationController(rootViewController: LocationListViewController())
+
+        mapVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 1)
+        listVC.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 1)
+
+        viewControllers = [
+            mapVC,
+            listVC
+        ]
+
+        selectedIndex = 0
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
